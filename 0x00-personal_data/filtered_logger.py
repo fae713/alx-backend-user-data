eebc8edbe8da795d54543a577cc96073714e2ff5
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-07-03 16:10:40 +0000

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,22 +2,41 @@
 """
 Regex-ing
 """
+import logging
 import re
 
-
-def filter_datum(fields, redaction, message, separator):
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class.
     """
-    A function called that returns the log message obfuscated.
 
-    Args:
-        fields (list): List of field names to obfuscate.
-        redaction (str): String to replace the fields with.
-        message (str): Log message to obfuscate.
-        separator (str): Character separating fields in the message.
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
 
-    Returns:
-        str: Obfuscated log message.
-    """
-    pattern = f'({"|".join(fields)}){separator}[^{separator}]*'
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields_to_redact = fields
 
-    return re.sub(pattern, f'{redaction}{separator}', message)
+    def format(self, record: logging.LogRecord) -> str:
+        org_msg = super().format(record)
+        filtered_msg = self.filter_datum(
+            self.fields_to_redact, self.REDACTION, org_msg, self.SEPARATOR)
+        return filtered_msg
+
+    @staticmethod
+    def filter_datum(fields, redaction, message, separator):
+        """
+        A function called that returns the log message obfuscated.
+
+        Args:
+            fields (list): List of field names to obfuscate.
+            redaction (str): String to replace the fields with.
+            message (str): Log message to obfuscate.
+            separator (str): Character separating fields in the message.
+
+        Returns:
+            str: Obfuscated log message.
+        """
+        pattern = f'({"|".join(fields)}){separator}[^{separator}]*'
+
+        return re.sub(pattern, f'{redaction}{separator}', message)
