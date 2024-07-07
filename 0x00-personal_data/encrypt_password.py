81bb62bcf6c138c6a837f6f55416699657d37607
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-07-07 16:33:42 +0000

    Debugged and completed the tasks.

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+A module for encrypting passwords using bcrypt.
+"""
+import bcrypt
+
+
+SALT_ROUNDS = 12  # Default to 12 rounds.
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes a password using a random salt with a specified number of rounds.
+
+    Args:
+        password (str): The plaintext password to hash.
+
+    Returns:
+        bytes: The hashed password.
+
+    Raises:
+        TypeError: If the input password is not a string.
+        ValueError: If the password is empty.
+    """
+    if not isinstance(password, str):
+        raise TypeError("Password must be a string.")
+    if not password:
+        raise ValueError("Password cannot be empty.")
+
+    return bcrypt.hashpw(
+        password.encode('utf-8'), bcrypt.gensalt(rounds=SALT_ROUNDS))
+
+
+def is_valid_password(hashed_password: bytes, password: str) -> bool:
+    """
+    Checks if a hashed password matches the given plaintext password.
+
+    Args:
+        hashed_password (bytes): The hashed password to compare against.
+        password (str): The plaintext password to validate.
+
+    Returns:
+        bool: True if the password matches the hash, False otherwise.
+
+    Raises:
+        TypeError: If the hashed_password is not bytes
+                   or password is not a string.
+
+        ValueError: If the password is empty.
+    """
+    if not isinstance(hashed_password, bytes):
+        raise TypeError("hashed_password must be bytes.")
+    if not isinstance(password, str):
+        raise TypeError("Password must be a string.")
+    if not password:
+        raise ValueError("Password cannot be empty.")
+
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,42 +1,99 @@
 #!/usr/bin/env python3
 """
-Regex-ing
+Filtered logger module.
 """
-import logging
+
+import os
 import re
+import logging
+import mysql.connector
+from typing import List
+
+# Load environment variables at the start
+db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
+db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'
+    .format('|'.join(map(re.escape, x)), y),
+
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction:
+                 str, message: str, separator: str) -> str:
+    """
+    A function that returns unclear log messages.
+    """
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
+
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class.
-    """
+    """Redacting Formatter class"""
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    FORMAT_FIELDS = ('name', 'levelname', 'asctime', 'message')
     SEPARATOR = ";"
 
-    def __init__(self, fields):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields_to_redact = fields
+    def __init__(self, fields: List[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        org_msg = super().format(record)
-        filtered_msg = self.filter_datum(
-            self.fields_to_redact, self.REDACTION, org_msg, self.SEPARATOR)
-        return filtered_msg
+        """Formats a LogRecord."""
+        msg = super().format(record)
+        txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return txt
 
-    @staticmethod
-    def filter_datum(fields, redaction, message, separator):
-        """
-        A function called that returns the log message obfuscated.
 
-        Args:
-            fields (list): List of field names to obfuscate.
-            redaction (str): String to replace the fields with.
-            message (str): Log message to obfuscate.
-            separator (str): Character separating fields in the message.
+# Setup logger
+logger = logging.getLogger("user_data")
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+logger.setLevel(logging.INFO)
+logger.propagate = False
+logger.addHandler(stream_handler)
 
-        Returns:
-            str: Obfuscated log message.
-        """
-        pattern = f'({"|".join(fields)}){separator}[^{separator}]*'
 
-        return re.sub(pattern, f'{redaction}{separator}', message)
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Creates a connector to a database."""
+    connection = mysql.connector.connect(
+        host=db_host,
+        port=3306,
+        user=db_user,
+        password=db_pwd,
+        database=db_name,
+    )
+    return connection
+
+
+def main():
+    """Logs the information about user records in a table."""
+    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
+    columns = fields.split(',')
+    query = "SELECT {} FROM users;".format(fields)
+    connection = get_db()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            for row in rows:
+                record = map(lambda x: '{}={}'
+                             .format(x[0], x[1]), zip(columns, row))
+                msg = '{};'.format('; '.join(list(record)))
+                args = ("user_data", logging.INFO, None, None, msg, None, None)
+                log_record = logging.LogRecord(*args)
+                logger.handle(log_record)
+    except Exception as e:
+        logger.error(f"Database operation failed: {e}")
+
+
+if __name__ == "__main__":
+    main()
