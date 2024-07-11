7165c7b4bead1f086c5838a2b2b13755dd75606b
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-07-11 15:19:11 +0000

    task 5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -41,7 +41,9 @@ class Auth:
         """
         The method that extracts the authorization header from request.
         """
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> User:
         """
