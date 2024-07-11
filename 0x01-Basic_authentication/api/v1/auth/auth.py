e47eae14038175299d7b4a00c4335cf39aa27bbd
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-07-11 14:14:06 +0000

    task 3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,6 +9,7 @@ from typing import List, TypeVar
 
 User = TypeVar('User')
 
+
 class Auth:
     """
     A class to manage the API authentication.
@@ -19,20 +20,20 @@ class Auth:
         the class init.
         """
         pass
-    
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         A method that takes care of the required auth.
         """
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """
         The method that extracts the authorization header from request.
         """
         return None
-    
-    def current_user(self, request=None) -> User: 
+
+    def current_user(self, request=None) -> User:
         """
         The method that gets the current user from the request.
         """
