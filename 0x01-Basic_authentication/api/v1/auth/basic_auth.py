69a7081ba6e24ba41c8e40dcfc21b124e643cc93
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-07-11 15:35:23 +0000

    task 6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Basic Auth
+"""
+
+from flask import request
+from typing import List, TypeVar
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth:
+    """
+    A basic auth class.
+    """
+    pass
