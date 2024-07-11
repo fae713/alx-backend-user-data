d74915bccf2816123a758ab3f305f75de04a5476
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-07-11 15:59:52 +0000

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,4 +12,23 @@ class BasicAuth(Auth):
     """
     A basic auth class.
     """
-    pass
+    def __init__(self) -> None:
+        super().__init__()
+        pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        This class extracts the base64 auth part from the
+        authorization hearder.
+        """
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header.split(" ", 1)[1]
