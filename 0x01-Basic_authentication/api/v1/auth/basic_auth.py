72abf895ac91fa764357d96966c0b0eea8934405
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-07-11 16:22:31 +0000

    task 9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ Basic Auth
 
 import base64
 from flask import request
+from models.user import User
 from typing import List, TypeVar, Tuple
 from .auth import Auth
 
@@ -66,3 +67,22 @@ class BasicAuth(Auth):
             return None, None
         email, password = decoded_base64_authorization_header.split(":", 1)
         return email, password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> User:
+        """
+        Retrieve a User instance based on the provided email and password.
+        """
+
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        unidentified_users = User.search(email=user_email)
+
+        for user in unidentified_users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
