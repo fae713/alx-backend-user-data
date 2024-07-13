e09b23f9233727704ac03a1c292e8f514116fadc
fae713 <favourmichael47@gmail.com>
2024-07-13 12:02:46 +0100

    task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,10 +5,32 @@ Empty session
 
 from flask import request
 from .auth import Auth
+from typing import List
+import uuid
 
 
 class SessionAuth(Auth):
     """
     A class that creates a session suthentication.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def __init__(self) -> None:
+        super().__init__()
+        pass
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        A method that creates a Session ID for a user_id.
+        """
+        if user_id is None:
+            return None
+
+        if not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
