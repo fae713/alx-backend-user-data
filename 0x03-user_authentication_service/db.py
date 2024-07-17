3a4645cdc47667d534a3f988c0950fc3460253fc
fae713 <favourmichael47@gmail.com>
2024-07-17 18:37:03 +0100

    task 4

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,6 +2,7 @@
 """
 DB module
 """
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -24,6 +25,9 @@ class DB:
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
+
+        logging.getLogger(
+            'sqlalchemy.engine').setLevel(logging.WARNING)
 
     @property
     def _session(self) -> Session:
@@ -55,14 +59,30 @@ class DB:
         This method finds a user with kwargs in the DB.
         """
         try:
-            result = self._session.query(
-                User).filter_by(**kwargs).one_or_none()
+            result = self._session.query(User).filter_by(**kwargs).one_or_none()
 
             if result is None:
                 raise NoResultFound(f"No user found matching {kwargs}")
             elif result is not None:
                 return result
-        except MultipleResultsFound:
-            raise NoResultFound(f"Multiple users found matching {kwargs}")
-        except InvalidRequestError as e:
-            raise InvalidRequestError(f"Invalid request: {str(e)}")
+        except NoResultFound as e:
+            raise NoResultFound(f"No user found matching {kwargs}") from e
+    
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        Updates a user's attributes.
+        """
+        user = self.find_user_by(id=user_id)
+
+        if user is None:
+            raise ValueError(f"No user found with ID {user_id}")
+
+        # Update attributes
+        for attr_name, attr_value in kwargs.items():
+            if hasattr(User, attr_name):
+                setattr(user, attr_name, attr_value)
+            else:
+                raise ValueError(
+                    f"Invalid '{attr_name}'.")
+
+        self._session.commit()
