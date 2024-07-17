dc2eda33da1114c2e4dda06416f432c9c3cfba25
fae713 <favourmichael47@gmail.com>
2024-07-17 17:38:10 +0100

    task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
-from sqlalchemy.orm.exc import InvalidRequestError
+from sqlalchemy.exc import InvalidRequestError
 from user import User
 from user import Base
 
