9c28aae1f14427c9ad6e40f97db5132647887ddd
fae713 <favourmichael47@gmail.com>
2024-07-18 23:06:01 +0100

    task 9

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,14 @@ import bcrypt
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 from user import User
+from uuid import uuid4
+
+
+def _generate_uuid() -> str:
+    """
+    Generates a user unique ID.
+    """
+    return str(uuid4())
 
 
 def _hash_password(password: str) -> bytes:
