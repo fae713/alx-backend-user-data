9e16615ff27dcd90e51acb9ac830d4bad747559f
fae713 <favourmichael47@gmail.com>
2024-07-18 21:56:34 +0100

    task 5

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Hash Password
+"""
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """
+    A method that takes in a password string arguments
+    and returns bytes.
+    """
+    pw_bytes = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(pw_bytes, salt)
+
+    return hashed_password
