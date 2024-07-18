59f888afc355a2a7976df9a65b2b75a077670d28
fae713 <favourmichael47@gmail.com>
2024-07-18 22:45:45 +0100

    task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -18,6 +18,22 @@ def index() -> str:
     """
     return jsonify({"message": "Bienvenue"})
 
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """
+    POST '/users'
+    Return: The account creation payload.
+    """
+    email = request.form.get("email")
+
+    password = request.form.get("password")
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
